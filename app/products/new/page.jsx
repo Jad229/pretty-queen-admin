7712c2924abec785d4b147e8d00285e58b34394d
Jad229 <jad229@njit.edu>
2023-08-30 09:45:15 -0400

    firebase image upload

--- app/products/new/page.jsx
+++ app/products/new/page.jsx
@@ -1,18 +1,71 @@
 "use client";
 import InputComponent from "@components/FormElements/InputComponent";
-import { addProductFormControls } from "@utils";
+import {
+  addProductFormControls,
+  firebaseConfig,
+  firebaseStorageURL,
+} from "@utils";
 import axios from "axios";
+import { initializeApp } from "firebase/app";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { useState } from "react";
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app, firebaseStorageURL);
 
 function AddNewProduct() {
   const [formData, setFormData] = useState({
     name: "",
     price: "",
     type: "",
-    imageUrl: "link",
+    imageUrl: "",
     description: "",
   });
 
+  const createFileName = (file) => {
+    const timeStamp = Date.now();
+    const randomString = Math.random().toString(36).substring(2, 12);
+
+    return `${file.name}-${timeStamp}-${randomString}`;
+  };
+
+  async function firebaseImageUpload(file) {
+    const fileName = createFileName(file);
+    const storageReference = ref(storage, `productImages/${fileName}`);
+    const uploadImage = uploadBytesResumable(storageReference, file);
+
+    return new Promise((resolve, reject) => {
+      uploadImage.on(
+        "state_changed",
+        (snapshot) => {},
+        (error) => {
+          console.log(error);
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadImage.snapshot.ref)
+            .then((downloadUrl) => resolve(downloadUrl))
+            .catch((error) => reject(error));
+        }
+      );
+    });
+  }
+
+  async function handleImage(e) {
+    const extractedImageUrl = await firebaseImageUpload(e.target.files[0]);
+
+    if (extractedImageUrl !== "") {
+      setFormData({
+        ...formData,
+        imageUrl: extractedImageUrl,
+      });
+    }
+  }
   function onChange(e) {
     e.preventDefault();
 
@@ -20,10 +73,6 @@ function AddNewProduct() {
       ...prevFormData,
       [e.target.name]: e.target.value,
     }));
-  }
-
-  function handleImage() {
-    return null;
   }
 
   async function createProduct(e) {
--- utils/index.js
+++ utils/index.js
@@ -21,3 +21,14 @@ export const addProductFormControls = [
     componentType: "input",
   },
 ];
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC_A9lUTT8_-AC1dt0sYMvf_gfZ5rPs574",
+  authDomain: "prettyqueen-f2767.firebaseapp.com",
+  projectId: "prettyqueen-f2767",
+  storageBucket: "prettyqueen-f2767.appspot.com",
+  messagingSenderId: "526527637889",
+  appId: "1:526527637889:web:7faaed25f830be3e60e080",
+};
+
+export const firebaseStorageURL = "gs://prettyqueen-f2767.appspot.com";
